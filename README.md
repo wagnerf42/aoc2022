454cf74ba4ce287256d9055d100894127a5b4394
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2022-12-07 15:05:49 +0100

    day 7

---
+++ README.md
@@ -0,0 +1,8 @@
+aoc 2022
+--------
+
+notable days :
+
+1) good example of why itertools rocks
+5) good example of double borrow
+7) good example where ownership is a pain
--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,18 +1,15 @@
-use itertools::Itertools;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
+    path::PathBuf,
     str::FromStr,
 };
 
+#[derive(Debug)]
 enum Entry {
-    File(u32, String),
-    Dir(String),
-}
-
-struct Dir {
-    absolute_name: String,
-    entries: Vec<Entry>,
+    File(u32, PathBuf),
+    Dir(PathBuf),
 }
 
 enum InputLine {
@@ -30,13 +27,13 @@ impl FromStr for Entry {
             tokens
                 .next()
                 .ok_or(())
-                .map(|dirname| Entry::Dir(dirname.to_string()))
+                .map(|dirname| Entry::Dir(dirname.into()))
         } else {
             start.parse::<u32>().map_err(|_| ()).and_then(|size| {
                 tokens
                     .next()
                     .ok_or(())
-                    .map(|filename| Entry::File(size, filename.to_string()))
+                    .map(|filename| Entry::File(size, filename.into()))
             })
         }
     }
@@ -49,17 +46,74 @@ impl FromStr for InputLine {
         if s.starts_with("$ cd") {
             Ok(InputLine::Cd(s[5..].to_string()))
         } else if s.starts_with("$ ls") {
-            Err(())
+            Err(()) // useless, we just discard it through err
         } else {
             s.parse::<Entry>().map(InputLine::Entry)
         }
     }
 }
 
+fn fill_sizes(
+    sizes: &mut HashMap<PathBuf, u32>,
+    dirs: &HashMap<PathBuf, Vec<Entry>>,
+    current_path: &PathBuf,
+) -> u32 {
+    sizes.get(current_path).cloned().unwrap_or_else(|| {
+        let computed_size = dirs
+            .get(current_path)
+            .map(|entries| {
+                entries
+                    .iter()
+                    .map(|e| match e {
+                        Entry::File(size, _) => *size,
+                        Entry::Dir(dir_name) => {
+                            let mut absolute_dir = current_path.to_owned();
+                            absolute_dir.push(dir_name);
+                            fill_sizes(sizes, dirs, &absolute_dir)
+                        }
+                    })
+                    .sum::<u32>()
+            })
+            .unwrap();
+        sizes.insert(current_path.to_owned(), computed_size);
+        computed_size
+    })
+}
+
 fn main() -> std::io::Result<()> {
-    BufReader::new(File::open("input")?)
+    let (_, dirs): (_, HashMap<PathBuf, Vec<Entry>>) = BufReader::new(File::open("input")?)
         .lines()
         .filter_map(|l| l.ok())
-        .filter_map(|l| l.parse::<InputLine>().ok());
+        .filter_map(|l| l.parse::<InputLine>().ok())
+        .fold((PathBuf::new(), HashMap::new()), |(mut cwd, mut h), l| {
+            match l {
+                InputLine::Cd(path) => match path.as_str() {
+                    "/" => cwd.clear(),
+                    ".." => {
+                        cwd.pop();
+                    }
+                    s => {
+                        cwd.push(s);
+                    }
+                },
+                InputLine::Entry(e) => h.entry(cwd.clone()).or_default().push(e),
+            }
+            (cwd, h)
+        });
+    let mut sizes = HashMap::new();
+    let root = PathBuf::new();
+    fill_sizes(&mut sizes, &dirs, &root);
+    let s: u32 = sizes.values().filter(|&&s| s <= 100_000).sum();
+    println!("sum of dirs < 100_000 : {}", s);
+
+    // note that we could be more optimal by walking the tree
+    // but we still need to walk the whole tree to compute the sizes in the first place
+    let free_space = 70_000_000 - sizes.get(&root).unwrap();
+    let s: u32 = *sizes
+        .values()
+        .filter(|&&s| free_space + s >= 30_000_000)
+        .min()
+        .unwrap();
+    println!("min dir large enough: {}", s);
     Ok(())
 }

0bad126a7e32e7da65e70cdf34d4caeb7dbfef8a
frederic wagner <frederic.wagner@imag.fr>
2022-12-08 11:15:52 +0100

    day 8

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ notable days :
 1) good example of why itertools rocks
 5) good example of double borrow
 7) good example where ownership is a pain
+8) needs dyn ptr because we need to apply the same op to iter and iter.rev
--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,20 +1,64 @@
 use itertools::Itertools;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
 
-fn previous_max<'a, I: Iterator<Item = (usize, usize)> + 'a>(
+#[derive(Default, Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+fn previous_max<'a, I: Iterator<Item = Pos> + 'a>(
     grid: &'a [Vec<i8>],
     iter: I,
-) -> impl Iterator<Item = (i8, usize, usize, i8)> + 'a {
-    iter.scan(-1, |max, (y, x)| {
-        let d = grid[y][x];
+) -> impl Iterator<Item = (i8, Pos, i8)> + 'a {
+    iter.scan(-1, |max, pos| {
+        let d = grid[pos.y][pos.x];
         let previous_max = *max;
         *max = previous_max.max(d);
-        Some((previous_max, y, x, d))
+        Some((previous_max, pos, d))
     })
-    .take_while(|&(m, _, _, _)| m < 9)
+    .take_while(|&(m, _, _)| m < 9)
+}
+
+fn previous_limit<'a, I: Iterator<Item = Pos> + 'a>(
+    grid: &'a [Vec<i8>],
+    iter: I,
+) -> impl Iterator<Item = (Pos, Pos)> + 'a {
+    let mut i = iter.peekable();
+    let first_pos = i.peek().copied().unwrap();
+    i.scan(vec![(10, first_pos)], |previous_highers, pos| {
+        let d = grid[pos.y][pos.x];
+        let mut sightblocking = None;
+        while previous_highers
+            .last()
+            .map(|&(height, _)| height <= d)
+            .unwrap()
+        {
+            let (height, previous_pos) = previous_highers.pop().unwrap();
+            if height == d {
+                sightblocking = Some(previous_pos)
+            }
+        }
+        if sightblocking.is_none() {
+            sightblocking = previous_highers.last().map(|&(_, pos)| pos);
+        }
+        previous_highers.push((d, pos));
+        Some((sightblocking.unwrap(), pos))
+    })
+}
+
+impl Pos {
+    fn distance(&self, other: &Self) -> usize {
+        let xmin = self.x.min(other.x);
+        let xmax = self.x.max(other.x);
+        let ymin = self.y.min(other.y);
+        let ymax = self.y.max(other.y);
+        (xmax - xmin) + (ymax - ymin)
+    }
 }
 
 fn main() -> std::io::Result<()> {
@@ -32,15 +76,15 @@ fn main() -> std::io::Result<()> {
     let columns = grid.get(0).map(|l| l.len()).unwrap_or_default();
 
     let mut left_right =
-        (0..lines).flat_map(|y| previous_max(&grid, (0..columns).map(move |x| (y, x))));
+        (0..lines).flat_map(|y| previous_max(&grid, (0..columns).map(move |x| Pos { x, y })));
     let mut top_bottom =
-        (0..columns).flat_map(|x| previous_max(&grid, (0..lines).map(move |y| (y, x))));
+        (0..columns).flat_map(|x| previous_max(&grid, (0..lines).map(move |y| Pos { x, y })));
     let mut right_left =
-        (0..lines).flat_map(|y| previous_max(&grid, (0..columns).map(move |x| (y, x)).rev()));
+        (0..lines).flat_map(|y| previous_max(&grid, (0..columns).map(move |x| Pos { x, y }).rev()));
     let mut bottom_top =
-        (0..columns).flat_map(|x| previous_max(&grid, (0..lines).map(move |y| (y, x)).rev()));
+        (0..columns).flat_map(|x| previous_max(&grid, (0..lines).map(move |y| Pos { x, y }).rev()));
 
-    let mut iterators: [&mut dyn Iterator<Item = (i8, usize, usize, i8)>; 4] = [
+    let mut iterators: [&mut dyn Iterator<Item = (i8, Pos, i8)>; 4] = [
         &mut left_right,
         &mut top_bottom,
         &mut right_left,
@@ -50,12 +94,41 @@ fn main() -> std::io::Result<()> {
     let visible = iterators
         .iter_mut()
         .flatten()
-        .filter(|&(max, _, _, d)| max < d)
-        .map(|(_, y, x, _)| (y, x))
+        .filter(|&(max, _, d)| max < d)
+        .map(|(_, p, _)| p)
         .unique()
         .count();
 
     println!("visible {}", visible);
+
+    let mut left_right =
+        (0..lines).flat_map(|y| previous_limit(&grid, (0..columns).map(move |x| Pos { x, y })));
+    let mut top_bottom =
+        (0..columns).flat_map(|x| previous_limit(&grid, (0..lines).map(move |y| Pos { x, y })));
+    let mut right_left = (0..lines)
+        .flat_map(|y| previous_limit(&grid, (0..columns).map(move |x| Pos { x, y }).rev()));
+    let mut bottom_top = (0..columns)
+        .flat_map(|x| previous_limit(&grid, (0..lines).map(move |y| Pos { x, y }).rev()));
+
+    let mut iterators: [&mut dyn Iterator<Item = (Pos, Pos)>; 4] = [
+        &mut left_right,
+        &mut top_bottom,
+        &mut right_left,
+        &mut bottom_top,
+    ];
+
+    let scores = iterators
+        .iter_mut()
+        .flatten()
+        .map(|(previous_pos, pos)| (previous_pos.distance(&pos), pos))
+        .fold(HashMap::new(), |mut scores, (distance, pos)| {
+            *scores.entry(pos).or_insert(1) *= distance;
+            scores
+        });
+
+    let best_score = scores.values().copied().max().unwrap_or_default();
+
+    println!("best scenic score {}", best_score);
 
     Ok(())
 }

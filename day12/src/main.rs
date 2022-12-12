f8f1ef6e6f4458854356ed7d7554a8241cc6c482
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2022-12-12 14:08:52 +0100

    day 12

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,8 +1,8 @@
 use std::{
+    collections::{HashSet, VecDeque},
     fs::File,
+    hash::Hash,
     io::{BufRead, BufReader},
-    iter::Chain,
-    option::IntoIter,
     path::Path,
 };
 
@@ -24,17 +24,14 @@ fn load_map<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<Vec<u8>>> {
         .collect())
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Pos {
     x: usize,
     y: usize,
 }
 
 impl Pos {
-    fn neighbours(
-        &self,
-        xmax: usize,
-        ymax: usize,
-    ) -> Chain<Chain<Chain<IntoIter<Pos>, IntoIter<Pos>>, IntoIter<Pos>>, IntoIter<Pos>> {
+    fn neighbours(&self, xmax: usize, ymax: usize) -> impl Iterator<Item = Pos> {
         self.x
             .checked_sub(1)
             .map(|x| Pos { x, y: self.y })
@@ -51,58 +48,78 @@ impl Pos {
     }
 }
 
-fn height_diff(grid: &[Vec<u8>], pos1: &Pos, pos2: &Pos) -> u8 {
-    todo!()
+fn height_diff(grid: &[Vec<u8>], pos1: &Pos, pos2: &Pos) -> i8 {
+    let h1 = grid[pos1.y][pos1.x];
+    let h2 = grid[pos2.y][pos2.x];
+    h2 as i8 - h1 as i8
 }
 
-struct NFilter<'g, G: Graph, I> {
-    graph: &'g G,
-    iter: I,
-    n1: &'g G::Vertex,
-}
-
-impl<'g, G: Graph, I: Iterator<Item = G::Vertex>> Iterator for NFilter<'g, G, I> {
-    type Item = I::Item;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.iter
-            .next()
-            .filter(|n2| self.graph.are_neighbours(self.n1, n2))
-    }
-}
-
-trait Graph {
-    type Vertex;
-    type Neighbours<'b>
-    where
-        Self: 'b;
-    fn are_neighbours(&self, v1: &Self::Vertex, v2: &Self::Vertex) -> bool;
-    fn neighbours<'b>(&'b self, vertex: &'b Self::Vertex) -> Self::Neighbours<'b>;
-}
-
-impl Graph for Vec<Vec<u8>> {
-    type Vertex = Pos;
-    type Neighbours<'b> = NFilter<
-        'b,
-        Self,
-        Chain<Chain<Chain<IntoIter<Pos>, IntoIter<Pos>>, IntoIter<Pos>>, IntoIter<Pos>>,
-    >;
-    fn neighbours<'b>(&'b self, vertex: &'b Self::Vertex) -> Self::Neighbours<'b> {
-        let ymax = self.len();
-        let xmax = self[0].len();
-        NFilter {
-            graph: self,
-            iter: vertex.neighbours(xmax, ymax),
-            n1: vertex,
-        }
-    }
-
-    fn are_neighbours(&self, v1: &Self::Vertex, v2: &Self::Vertex) -> bool {
-        height_diff(self, v1, v2) <= 1
-    }
+fn find_pos(map: &[Vec<u8>], target: u8) -> Option<Pos> {
+    map.iter()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.iter()
+                .enumerate()
+                .map(move |(x, code)| (Pos { x, y }, code))
+        })
+        .find_map(|(pos, code)| if *code == target { Some(pos) } else { None })
 }
 
 fn main() -> std::io::Result<()> {
     let map = load_map("input")?;
+    let start = find_pos(&map, b'a' - 1).unwrap();
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0));
+    let mut seen = HashSet::new();
+    let distance_to_end = loop {
+        if let Some((pos, d)) = queue.pop_front() {
+            if seen.contains(&pos) {
+                continue;
+            }
+            seen.insert(pos);
+            if map[pos.y][pos.x] == b'z' + 1 {
+                break Some(d);
+            } else {
+                queue.extend(pos.neighbours(map[0].len(), map.len()).filter_map(|n| {
+                    if !seen.contains(&n) && height_diff(&map, &pos, &n) <= 1 {
+                        Some((n, d + 1))
+                    } else {
+                        None
+                    }
+                }))
+            }
+        } else {
+            break None;
+        }
+    };
+    println!("end is at {distance_to_end:?}");
+
+    let start = find_pos(&map, b'z' + 1).unwrap();
+    let mut queue = VecDeque::new();
+    queue.push_back((start, 0));
+    let mut seen = HashSet::new();
+    let distance_to_end = loop {
+        if let Some((pos, d)) = queue.pop_front() {
+            if seen.contains(&pos) {
+                continue;
+            }
+            seen.insert(pos);
+            if map[pos.y][pos.x] <= b'a' {
+                break Some(d);
+            } else {
+                queue.extend(pos.neighbours(map[0].len(), map.len()).filter_map(|n| {
+                    if !seen.contains(&n) && height_diff(&map, &pos, &n) >= -1 {
+                        Some((n, d + 1))
+                    } else {
+                        None
+                    }
+                }))
+            }
+        } else {
+            break None;
+        }
+    };
+    println!("end is at {distance_to_end:?}");
+
     Ok(())
 }
